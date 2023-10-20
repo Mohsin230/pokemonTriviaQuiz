bfa6fbb9f08e1c733ca016795b313bdf8ba03686
mohsin malik <76977462+Mohsin230@users.noreply.github.com>
2023-10-20 11:21:20 -0400

    Add files via upload

---
+++ pokemonTriva.js
@@ -0,0 +1,147 @@
+const questions = [
+    {
+        question: "What type is Pikachu?",
+        options: ["Electric", "Fire", "Water", "Grass"],
+        answer: "Electric"
+    },
+    {
+        question: "Which Pokémon evolves from Eevee when leveled up near an Ice Rock?",
+        options: ["Vaporeon", "Jolteon", "Flareon", "Glaceon"],
+        answer: "Glaceon"
+    },
+    {
+        question: "Which Legendary Pokémon is known as the 'Time Traveler'?",
+        options: ["Arceus", "Dialga", "Palkia", "Giratina"],
+        answer: "Dialga"
+    },
+    {
+        question: "Which move can Charizard not learn by leveling up?",
+        options: ["Flamethrower", "Thunder Punch", "Dragon Claw", "Earthquake"],
+        answer: "Earthquake"
+    },
+    {
+        question: "Which of the following Pokémon is not a Water type?",
+        options: ["Squirtle", "Bulbasaur", "Totodile", "Mudkip"],
+        answer: "Bulbasaur"
+    },
+    {
+        question: "What is the main type of Meowth, the Pokémon known for its love of money?",
+        options: ["Normal", "Dark", "Psychic", "Fighting"],
+        answer: "Normal"
+    },
+    {
+        question: "Which Pokémon is known for having multiple evolutions depending on its friendship with its Trainer?",
+        options: ["Eevee", "Pikachu", "Clefairy", "Ralts"],
+        answer: "Eevee"
+    },
+    {
+        question: "What is the name of the organization that aims to steal rare Pokémon in the Pokémon series?",
+        options: ["Team Magma", "Team Aqua", "Team Rocket", "Team Galactic"],
+        answer: "Team Rocket"
+    },
+    {
+        question: "Which Pokémon is known for its ability to Mega Evolve into two different forms?",
+        options: ["Charizard", "Blastoise", "Venusaur", "Gengar"],
+        answer: "Charizard"
+    },
+    {
+        question: "Which region is the setting for the Pokémon Sun and Moon games?",
+        options: ["Kanto", "Johto", "Alola", "Unova"],
+        answer: "Alola"
+    },
+    {
+        question: "In Pokémon lore, what is the name of the ancient war between Dialga, Palkia, and Giratina?",
+        options: ["The Time War", "The Sinnoh Conflict", "The Creation Battle", "The Ancient Clash"],
+        answer: "The Sinnoh Conflict"
+    },
+    {
+        question: "Which move is the signature move of the Pokémon Rayquaza?",
+        options: ["Outrage", "Dragon Claw", "Draco Meteor", "Hyper Beam"],
+        answer: "Dragon Claw"
+    },
+    {
+        question: "Which Pokémon is known for being able to Mega Evolve without a Mega Stone?",
+        options: ["Mewtwo", "Lucario", "Rayquaza", "Charizard"],
+        answer: "Rayquaza"
+    },
+    {
+        question: "In Pokémon Black and White, which organization seeks to 'liberate' Pokémon from trainers?",
+        options: ["Team Rocket", "Team Magma", "Team Plasma", "Team Galactic"],
+        answer: "Team Plasma"
+    },
+    {
+        question: "What is the name of the main protagonist in the Pokémon Colosseum game?",
+        options: ["Red", "Wes", "Ethan", "Brendan"],
+        answer: "Wes"
+    },
+    {
+        question: "Which Pokémon type is weak to both Ground and Psychic-type moves?",
+        options: ["Fighting", "Rock", "Steel", "Poison"],
+        answer: "Poison"
+    },
+    {
+        question: "In Pokémon Sun and Moon, what is the name of the evil team led by Lusamine?",
+        options: ["Team Skull", "Team Aether", "Team Galactic", "Team Flare"],
+        answer: "Team Aether"
+    },
+    {
+        question: "Which of the following Pokémon has the highest base HP stat?",
+        options: ["Blissey", "Snorlax", "Chansey", "Wigglytuff"],
+        answer: "Blissey"
+    },
+    {
+        question: "In Pokémon Red and Blue, which move is known as the 'ultimate' Psychic-type attack?",
+        options: ["Psychic", "Psybeam", "Psych Up", "Psywave"],
+        answer: "Psychic"
+    },
+    {
+        question: "What is the name of the first Pokémon you encounter in the Pokémon Snap game for Nintendo 64?",
+        options: ["Pikachu", "Charizard", "Bulbasaur", "Meowth"],
+        answer: "Meowth"
+    },
+    {
+        question: "Which Pokémon game introduced the concept of Mega Evolution?",
+        options: ["Pokémon Ruby and Sapphire", "Pokémon Black and White", "Pokémon X and Y", "Pokémon Sun and Moon"],
+        answer: "Pokémon X and Y"
+    },
+    {
+        question: "In the Pokémon Trading Card Game, what is the maximum number of Prize Cards a player can have?",
+        options: ["4", "6", "8", "10"],
+        answer: "6"
+    },
+    {
+        question: "Which Pokémon is known for its ability to change forms using the DNA Splicers?",
+        options: ["Zygarde", "Deoxys", "Genesect", "Kyurem"],
+        answer: "Kyurem"
+    },
+    {
+        question: "What is the name of the professor in the Pokémon Mystery Dungeon games?",
+        options: ["Professor Oak", "Professor Elm", "Professor Sycamore", "Professor Mew"],
+        answer: "Professor Mew"
+    },
+    {
+        question: "Which Pokémon type is completely immune to Normal-type moves?",
+        options: ["Steel", "Ghost", "Fairy", "Dark"],
+        answer: "Ghost"
+    },
+    {
+        question: "In the Pokémon game 'Pokémon Conquest,' which type of creatures do you not capture but instead recruit?",
+        options: ["Pokémon", "Warlords", "Nobunaga's Army", "Wild Pokémon"],
+        answer: "Warlords"
+    },
+    {
+        question: "What is the name of the first Pokémon in the National Pokédex?",
+        options: ["Bulbasaur", "Pikachu", "Rattata", "Charmander"],
+        answer: "Bulbasaur"
+    },
+    {
+        question: "In the Pokémon game 'Pokémon XD: Gale of Darkness,' what is the name of the main character?",
+        options: ["Michael", "Lucas", "Ethan", "Brendan"],
+        answer: "Michael"
+    },
+    {
+        question: "Which Legendary Pokémon has the title 'Guardian of the Seas'?",
+        options: ["Lugia", "Kyogre", "Suicune", "Manaphy"],
+        answer: "Suicune"
+    }
+];
